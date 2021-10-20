9f6291080551497f58aa3b41759d4fecef50bf90
¨alemilera¨ <aleoriginal1016@gmail.com>
2021-10-20 18:51:25 -0300

    añadiendo navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+
+
+const Navbar = ()=>{
+    return(
+       <div className = "container">
+            <div className = "row ">
+            <div className = "col md-4">
+                <Link className = "btn btn-primary"  to = '/counter'>Counter</Link>
+            </div>
+            <div className = "col md-4">
+                <Link className = "btn btn-primary" to = '/students'>Students</Link>
+            </div>    
+        </div>
+       </div>
+    );
+}
+
+export default Navbar;
